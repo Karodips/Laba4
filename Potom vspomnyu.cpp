d8a19a36c1ee02a0ea22c5ae5cfe3b51f451a6b3
Karodips <54810893+Karodips@users.noreply.github.com>
2019-09-10 16:01:47 +0300

    Create Potom vspomnyu.cpp

---
+++ Potom vspomnyu.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    float a;
+    cout << "Введите значение A, которое написано перед π\n";
+    cin >> a;
+    if (a < 0 || a > 2) {
+        cout << "Вы не можете ввести значения ниже 0 или выше 2π";
+    }
+    else {
+        a = a*180;
+        cout << a << ""; 
+        
+    }
+
+    return 0;
+}
