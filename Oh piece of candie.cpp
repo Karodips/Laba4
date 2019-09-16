eab7cd2dd77a144337fbcd77aec94719259e932f
Karodips <54810893+Karodips@users.noreply.github.com>
2019-09-16 13:12:56 +0300

    Update Oh piece of candie.cpp

--- Oh piece of candie.cpp
+++ Oh piece of candie.cpp
@@ -3,14 +3,36 @@
 using namespace std;
 int main()
 {
-  double x, a, y,i;
-  cout << "Enter weight of your candies\n";
-  cin >> a;
-  cout << "Enter their price\n";
-  cin >> x;
-  cout << "How much candies do you want?\n";
-  cin >> y;
-  i = x/a;
-  cout << i << " " << i*y;
-  return 0;
+	double x, a, y, i;
+	cout << "Enter weight of your candies\n";
+	cin >> a;
+	if (a > 0) {
+		cout << "Enter their price\n";
+		cin >> x;
+		if (x > 0) {
+			cout << "How much candies do you want?\n";
+			cin >> y;
+			if (y >= 0) {
+				i = x / a;
+				cout << i << " " << i * y << "\n";
+			}
+			else {
+				cout << "incorrect amount";
+			}
+
+		}
+		else if (x == 0)
+		{
+			cout << "0 0" << "\n";
+		}
+		else {
+			cout << "incorrect amount";
+		}
+
+	}
+	else {
+		cout << "incorrect amount";
+	}
+	system("pause");
+	return 0;
 }
