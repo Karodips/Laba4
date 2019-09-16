eb7990bbafcd4c777730af5123e44cd9abd8c63a
Karodips <54810893+Karodips@users.noreply.github.com>
2019-09-16 13:41:35 +0300

    Update and rename Dodelay.cpp to Mashinki.cpp

--- Mashinki.cpp
+++ Mashinki.cpp
@@ -8,16 +8,22 @@ int main()
 	double x, a, y, i, g;
 	cout << "Введите скорость первого а зетем второго автомобиля.\n";
 	cin >> a >> x;
-	if (a*x > 0) {
 		cout << "Введите расстояние между ними\n";
 		cin >> y;
-		if (y > 0) {
+		if (y >= 0) {
 			cout << "Введите время\n";
 			cin >> i;
-			g = abs(a * i - x * i);
-			cout << abs(y - g) << "\n";
+			if (i >= 0) {
+				g = abs(a * i - x * i);
+				cout << abs(y - g) << "\n";
+			}
+			else {
+				cout << "Время не может быть отрицательным\n";
+			}
 		}
-	}
+		else {
+			cout << "Расстояние не может быть отрицательным\n";
+		}
 	system("pause");
 	return 0;
 }
